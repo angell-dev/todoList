9bd66124b81bf90fa463c94da47aa35302c8ab73
Angell <angell.dev071@gmail.com>
2022-09-24 17:34:52 -0300

    init project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './src/components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Today`s tasks</Text>
+
+        <View style={styles.items}>
+          <Task text='Task 1'/>
+          <Task text='Task 2'/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  wrapper:{
+   paddingTop: 80,
+   paddingHorizontal: 20,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: '700',
+    marginVertical: 22,
+  },
+  items:{},
 });
